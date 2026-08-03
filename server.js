83eaa6807982e0c095beb475df0fafd0223ed5e0
kannamnaidureddy <kannamnaidureddy@gmail.com>
2026-08-03 21:57:49 +0530

    Updated server status message

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ app.get("/api/users", async (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
